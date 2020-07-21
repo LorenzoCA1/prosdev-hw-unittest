b9025e1dca2596591145822d657ab7b43d530a1c
lorenzoavancena <lorenzo>
2020-07-21 16:30:11 +0800

    finish

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
 const functions = {
   gradeClassifier: (grade) => {
     // TODO: Write code here
+    const gradetable = [{letter: 'O', Description: 'Outstanding'},
+                        {letter: 'V', Description: 'Very Good'},
+                        {letter: 'G', Description: 'Good'},
+                        {letter: 'S', Description: 'Satisfactory'},
+                        {letter: 'N', Description: 'Needs Improvement'},
+                        {letter: 'D', Description: 'Did not Meet Expecation'},
+    ]
+
+    if(grade>100 || grade<0){
+        return false
+    }
+    if(isNaN(grade)){
+        return false
+    }
+    if(grade>=95.00&&grade<=100){
+        result = gradetable.find(el => el.letter === "O")
+        return {letter: result.letter, Description: result.Description}
+    }    
+    if(grade>=90.00&&grade<=94.99){
+    	result = gradetable.find(el => el.letter === "V")
+        return {letter: result.letter, Description: result.Description}
+    }    
+    if(grade>=85.00&&grade<=89.99){
+    	result = gradetable.find(el => el.letter === "G")
+        return {letter: result.letter, Description: result.Description}
+    }
+    if(grade>=80.00&&grade<=84.99){
+    	result = gradetable.find(el => el.letter === "S")
+        return {letter: result.letter, Description: result.Description}
+    }    
+    if(grade>=75.00&&grade<=79.99){
+    	result = gradetable.find(el => el.letter === "N")
+        return {letter: result.letter, Description: result.Description}
+    }    
+    if(grade>=0&&grade<=74.99){
+    	result = gradetable.find(el => el.letter === "D")
+        return {letter: result.letter, Description: result.Description}
+    }
   }
 }
 
--- src/index.test.js
+++ src/index.test.js
@@ -1,12 +1,66 @@
 const { gradeClassifier } = require('./index');
 
 // TODO: Write unit tests here
-describe('Scenario', () => {
-  it('When ..., then ...', () => {
-    // Arrange
+describe('GradeTable Scenario', () => {
+  it('When the grade given is not a number, then the result will return false', () => {
+  	//Arange
+  	let grade = 'a';
+  	//Act
+  	let result = gradeClassifier(grade);
+  	//Assert
+  	expect(result).toBe(false)
 
-    // Act
+  });
 
-    // Assert
+  it('When the grade given is over 100, then the result will return false', () => {
+  	//Arrange
+  	let grade = 120;
+  	//Act
+  	let result = gradeClassifier(grade);
+  	//Assert
+  	expect(result).toBe(false)
+
+  });
+
+  it('When the grade given is below 0, then the result will return false', () => {
+  	//Arrange
+  	let grade = -25;
+  	//Act
+  	let result = gradeClassifier(grade);
+  	//Assert
+  	expect(result).toBe(false)
+  });
+
+  it('When the grade given is between 0-100, then the result will return corresponding letter grade and description', () => {
+  	//Arrange
+  	let grade = 99;
+  	//Act
+  	let result = gradeClassifier(grade);
+  	//Assert
+  	if(grade>=95.00&&grade<=100){
+  		expect(result.letter).toBe('O');
+        expect(result.Description).toBe('Outstanding'); 
+    }
+    if(grade>=90.00&&grade<=94.99){
+    	expect(result.letter).toBe('V');
+        expect(result.Description).toBe('Very Good'); 
+    }    
+    if(grade>=85.00&&grade<=89.99){
+    	expect(result.letter).toBe('G');
+        expect(result.Description).toBe('Good'); 
+    }
+    if(grade>=80.00&&grade<=84.99){
+    	expect(result.letter).toBe('S');
+        expect(result.Description).toBe('Satisfactory'); 
+    }    
+    if(grade>=75.00&&grade<=79.99){
+    	expect(result.letter).toBe('N');
+        expect(result.Description).toBe('Needs Improvement'); 
+    }    
+    if(grade>=0&&grade<=74.99){
+    	expect(result.letter).toBe('D');
+        expect(result.Description).toBe('Did not Meet Expecation'); 
+    }    
+   
   });
 });
